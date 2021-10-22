181f8c9f13aabcd1f483732503e111ccdc92241e
SilverTiger34 <92964389+SilverTiger34@users.noreply.github.com>
2021-10-21 21:45:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function play1() {
+    document.getElementById("aud1").play()
+}
+
+function play2() {
+    document.getElementById("aud2").play()
+}
+
+function play3() {
+    document.getElementById("aud3").play()
+}
+
+function play4() {
+    document.getElementById("aud4").play()
+}
+
+function play5() {
+    document.getElementById("aud5").play()
+}
+
+function pause() {
+    document.getElementById("aud1").pause()
+
+    document.getElementById("aud2").pause()
+    document.getElementById("aud3").pause()
+    document.getElementById("aud4").pause()
+    document.getElementById("aud5").pause()
+}
